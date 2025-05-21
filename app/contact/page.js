7fb1a26fa446ce866affb81b651897fe99588091
KimZotic <hakimzotic@gmail.com>
2025-05-21 21:45:34 +0800

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-500 mb-4">Contact Us</h1>
+      <p>Email us at: support@meowboost.com</p>
+    </div>
+  )
+}
