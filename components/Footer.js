e4a91bef9df2af655560a41f73324f74b0e77b05
KimZotic <hakimzotic@gmail.com>
2025-05-22 16:30:25 +0800

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-zinc-950 text-white py-6 mt-10">
-      <div className="container mx-auto px-4 text-center text-sm">
-        &copy; {new Date().getFullYear()} MeowBoost. All rights reserved.
-      </div>
-    </footer>
-  );
-}
