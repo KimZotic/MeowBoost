081cc79beded6c7b06a668e61a57c5b2112701c4
KimZotic <hakimzotic@gmail.com>
2025-05-22 16:32:23 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "MeowBoost - Sosial Media Booster",
+  description: "Servis boost media sosial paling murah, aktif dan refill.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
